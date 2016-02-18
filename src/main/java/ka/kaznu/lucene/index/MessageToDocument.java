db601ea7ee681cc1a113927c7fb6b1dd4d391860
Sanzhar <sanzhar.aubakirov@innoforce.kz>
2016-02-18 20:00:35 +0600

    master: Create MessageToDocument class that we will use for creating Lucene Document from strings

---
+++ src/main/java/ka/kaznu/lucene/index/MessageToDocument.java
@@ -0,0 +1,32 @@
+package ka.kaznu.lucene.index;
+
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.FieldType;
+
+/**
+ * We will use this class to convert messages to Lucene documents
+ */
+public class MessageToDocument {
+
+    /**
+     * Creates Lucene Document using two strings: body and title
+     *
+     * @return resulted document
+     */
+    public static Document createWith(final String titleStr, final String bodyStr) {
+        final Document document = new Document();
+
+        final FieldType textIndexedType = new FieldType();
+        textIndexedType.setTokenized(true);
+
+        //index title
+        Field title = new Field("title", titleStr, textIndexedType);
+        //index body
+        Field body = new Field("body", bodyStr, textIndexedType);
+
+        document.add(title);
+        document.add(body);
+        return document;
+    }
+}
